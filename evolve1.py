9647a15e87421b1acf8f75ef9d9e81d965711a08
Stephan Richter <stephan.richter@gmail.com>
2004-12-09 20:12:47 +0000

    Provided some information text for this evolver.

--- evolve1.py
+++ evolve1.py
@@ -24,6 +24,12 @@ from zope.app.site.interfaces import ISite
 generation = 1
 
 def evolve(context):
+    """Evolve the ZODB from a Zope X3.0 to a X3.1 compatible format.
+
+    - Component-based registrations used to keep track of their components via
+      the component's path. Now it stores the component directly. All
+      registrations are updated to this new format.
+    """
     root = getRootFolder(context)
 
     # Fix up registration `componentPath` --> `component`
