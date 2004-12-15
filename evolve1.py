7f5f5d6ac7c243dab0cae0960279f7b89b0fa385
Stephan Richter <stephan.richter@gmail.com>
2004-12-15 23:52:41 +0000

    Restructured the error reporting service to become a utility.

--- evolve1.py
+++ evolve1.py
@@ -16,21 +16,63 @@
 $Id$
 """
 __docformat__ = "reStructuredText"
-from zope.app.zopeappgenerations import getRootFolder
+from zope.app import zapi
+from zope.app.error.error import ErrorReportingUtility
+from zope.app.error.interfaces import IErrorReportingService
+from zope.app.error.interfaces import IErrorReportingUtility
 from zope.app.generations.utility import findObjectsProviding
 from zope.app.registration.interfaces import IComponentRegistration
-from zope.app.site.interfaces import ISite 
+from zope.app.registration.interfaces import ActiveStatus, UnregisteredStatus
+from zope.app.site.interfaces import ISite, IServiceRegistration
+from zope.app.utility import UtilityRegistration
+from zope.app.zopeappgenerations import getRootFolder
 
 generation = 1
 
 def evolve(context):
     """Evolve the ZODB from a Zope X3.0 to a X3.1 compatible format.
 
+    - The Error Reporting Service was replaced by the Error Reporting
+      Utility. Thus, all service registrations have to be changed to utility
+      registrations. 
+
     - Component-based registrations used to keep track of their components via
       the component's path. Now it stores the component directly. All
       registrations are updated to this new format.
     """
     root = getRootFolder(context)
+
+    # Fix up Error Reporting Service --> Utility 
+    # We do this by simply removing old Error Reporting Services and their
+    # registrations and then add a new error reporting utility.
+    for site in findObjectsProviding(root, ISite):
+        for reg in findObjectsProviding(site.getSiteManager(),
+                                        IServiceRegistration):
+        
+            if reg.name == 'ErrorLogging':
+                errors = reg.component
+                # Set the registration to unregistered and then delete it
+                reg.status = UnregisteredStatus
+                del zapi.getParent(reg)[zapi.name(reg)]
+                # Get the properties from the old error reporting service and
+                # delete it
+                props = errors.getProperties()
+                folder = zapi.getParent(errors)
+                del folder[zapi.name(errors)]
+
+                # Only add a new error reporting utility, if there is none.
+                if 'ErrorReporting' not in folder:
+                    # Create the error reporting utility and set its properties
+                    utility = ErrorReportingUtility()
+                    utility.setProperties(**props)
+                    folder['ErrorReporting'] = utility
+                    # Register the utility and set the registration active
+                    reg = UtilityRegistration('', IErrorReportingUtility,
+                                              utility)
+                    reg_manager = folder.getRegistrationManager() 
+                    key = reg_manager.addRegistration(reg)
+                    reg_manager[key].status = ActiveStatus
+
 
     # Fix up registration `componentPath` --> `component`
     sites = findObjectsProviding(root, ISite)
