c8290f98188f25909bc90ca0e2baf34a202c262f
Stephan Richter <stephan.richter@gmail.com>
2004-12-21 16:39:25 +0000

    Converted Principal Annotation Service to be a utility. Added database evolution code to convert all service instances to utilities.

--- evolve1.py
+++ evolve1.py
@@ -21,6 +21,9 @@ from zope.app.error.error import ErrorReportingUtility
 from zope.app.error.interfaces import IErrorReportingService
 from zope.app.error.interfaces import IErrorReportingUtility
 from zope.app.generations.utility import findObjectsProviding
+from zope.app.principalannotation import PrincipalAnnotationUtility
+from zope.app.principalannotation.interfaces import IPrincipalAnnotationUtility
+from zope.app.principalannotation.interfaces import IPrincipalAnnotationService
 from zope.app.registration.interfaces import IComponentRegistration
 from zope.app.registration.interfaces import ActiveStatus, UnregisteredStatus
 from zope.app.site.interfaces import ISite, IServiceRegistration
@@ -32,6 +35,10 @@ generation = 1
 def evolve(context):
     """Evolve the ZODB from a Zope X3.0 to a X3.1 compatible format.
 
+    - The Principal Annotation Service was replaced by the Principal
+      Annotation Utility. Thus all service registrations have to be changed to
+      utility registrations. 
+
     - The Error Reporting Service was replaced by the Error Reporting
       Utility. Thus, all service registrations have to be changed to utility
       registrations. 
@@ -41,6 +48,43 @@ def evolve(context):
       registrations are updated to this new format.
     """
     root = getRootFolder(context)
+
+    # Fix up Principal Annotation Service --> Utility 
+    # We do this by simply removing old Principal Annotation Services and their
+    # registrations and then add a new Principal Annotation utility.
+    for site in findObjectsProviding(root, ISite):
+        for reg in findObjectsProviding(site.getSiteManager(),
+                                        IServiceRegistration):
+        
+            if reg.name == 'PrincipalAnnotation':
+                ann = reg.component
+                # Set the registration to unregistered and then delete it
+                reg.status = UnregisteredStatus
+                del zapi.getParent(reg)[zapi.name(reg)]
+                # Get the instance dictionary from the old principal
+                # annotation service and then delete the service
+                props = ann.__dict__
+                name = zapi.name(ann)
+                folder = zapi.getParent(ann)
+                del folder[name]
+
+
+                # Only add a new principal annotation utility, if there is none.
+                utils = [obj for obj in folder.values()
+                         if isinstance(obj, PrincipalAnnotationUtility)]
+                if len(utils) == 0:
+                    # Create the principal annotation utility and set its
+                    # properties
+                    utility = PrincipalAnnotationUtility()
+                    utility.__dict__.update(props)
+                    folder[name] = utility
+                    # Register the utility and set the registration active
+                    reg = UtilityRegistration('', IPrincipalAnnotationUtility,
+                                              utility)
+                    reg_manager = folder.getRegistrationManager() 
+                    key = reg_manager.addRegistration(reg)
+                    reg_manager[key].status = ActiveStatus
+
 
     # Fix up Error Reporting Service --> Utility 
     # We do this by simply removing old Error Reporting Services and their
